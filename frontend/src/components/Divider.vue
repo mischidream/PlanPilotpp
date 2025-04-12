e233cc3de9b8f6543d210326c49498c930128635
Michelle <michellekornherr@outlook.com>
2025-04-12 21:03:50 +0200

    Add divider component

---
+++ frontend/src/components/Divider.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="divider"></div>
+</template>
+  
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.divider {
+    height: 1px;
+    background-color: #ddd;
+    margin: 20px 0;
+}
+</style>
