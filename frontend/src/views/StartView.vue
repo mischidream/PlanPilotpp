e720aa7cb225392dadf0a963f0cc3791ef61a090
Michelle <michellekornherr@outlook.com>
2025-04-16 14:26:15 +0200

    Create StartView Layout

--- frontend/src/views/StartView.vue
+++ frontend/src/views/StartView.vue
@@ -1,11 +1,28 @@
 <template>
-    <h1>Start</h1>
+    <div class="input-fields">
+        <InputField label="Problem file:" v-model="instanceFile" type="file" accept=".pddl" />
+        <InputField label="Domain file:" v-model="domainFile" type="file" accept=".pddl" />
+        <Button label="Start" type="submit"></Button>
+    </div>
 </template>
 
 <script setup lang="ts">
+import InputField from '@/components/InputField.vue';
+import Button from '@/components/Button.vue';
+import { ref } from 'vue';
 
+const instanceFile = ref<File | null>(null);
+const domainFile = ref<File | null>(null);
 </script>
 
 <style scoped>
-
+.input-fields {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+.input-fields .button{
+    align-self: flex-end;
+    min-height: 40px;
+}
 </style>
