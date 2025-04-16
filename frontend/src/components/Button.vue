b5c56de42abb8924fc0033cfdb70e1cd8e5ca7f9
Michelle <michellekornherr@outlook.com>
2025-04-16 14:26:04 +0200

    Add Button component

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,42 @@
+<template>
+    <button
+      :type="type"
+      class="button"
+      @click="$emit('click')"
+    >
+      <slot>{{ label }}</slot>
+    </button>
+</template>
+  
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+  const props = defineProps({
+    label: {
+      type: String,
+      default: 'Click me',
+    },
+    type: {
+      type: String as PropType<'button' | 'submit' | 'reset'>,
+      default: 'button',
+    }
+  })
+  
+  defineEmits(['click'])
+</script>
+  
+<style scoped>
+  .button {
+    padding: 0.5rem 1rem;
+    background-color: var(--light-blue);
+    color: white;
+    border: none;
+    border-radius: var(--border-radius);
+    cursor: pointer;
+    min-width: 100px;
+  }
+  .button:hover {
+    background-color: var(--deep-blue);
+  }
+</style>
+  
