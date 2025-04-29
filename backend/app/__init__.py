86b7b54eda8d5af4799774194647221cdf4662ee
Michelle <michellekornherr@outlook.com>
2025-04-29 11:26:43 +0200

    Initialize Database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from persistence.db import db
+from persistence import models
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +8,14 @@ def create_app():
     # Register Blueprints for API endpoints
     from .rest.fastdownward import fastdownward_bp
     app.register_blueprint(fastdownward_bp, url_prefix='/api')
+
+    # Initialize database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ backend/app/persistence/models.py
@@ -0,0 +1,9 @@
+from .db import db
+
+class FastDownwardRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash_value = db.Column(db.String(64), unique=True, nullable=False)
+    domain_file_path = db.Column(db.String(512), nullable=False)
+    problem_file_path = db.Column(db.String(512), nullable=False)
+    sas_file_path = db.Column(db.String(512))
+    plan_file_path = db.Column(db.String(512))
