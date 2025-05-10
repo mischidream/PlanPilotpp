95d26181d3f13e35a4a582fa176aae8d9fb6c88a
Michelle <michellekornherr@outlook.com>
2025-05-10 15:18:32 +0200

    Added store to frontend

---
+++ frontend/src/stores/planStore.ts
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia';
+
+export const usePlanStore = defineStore('plan', {
+  state: () => ({
+    instanceFile: null as File | null,
+    domainFile: null as File | null,
+    horizon: 0,
+    sasFile: '',
+    planFile: '',
+  }),
+
+  actions: {
+    setFiles(instanceFile: File | null, domainFile: File | null) {
+      this.instanceFile = instanceFile;
+      this.domainFile = domainFile;
+    },
+    setInstanceFile(instanceFile: File | null){
+      this.instanceFile = instanceFile;
+      console.log(this.instanceFile);
+    },
+    setDomainFile(domainFile: File | null){
+      this.domainFile = domainFile;
+      console.log(this.domainFile);
+    },
+    setFastDownwardResponse(horizon: number, sasFile: string, planFile: string) {
+      this.horizon = horizon;
+      this.sasFile = sasFile;
+      this.planFile = planFile;
+    },
+  },
+});
